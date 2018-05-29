2fe63aab216a7c7ae61e792dfe3052d5d0759faf
Manny <epayano@itrsgroup.com>
2018-05-29 15:03:25 -0400

    PSP-23 updated the README.md, updated scripts, updated geneos directory

---
+++ scripts/PagerDuty_Incidents.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import requests, base64, json, sys, argparse, os
+from argparse import RawTextHelpFormatter
+
+# apiKey = "QiaeVM4257psyzkVSs5Q"
+authorization_token = 'QiaeVM4257psyzkVSs5Q'
+pagerduty_session = requests.Session()
+pagerduty_session.headers.update({
+  'Authorization': 'Token token=' + authorization_token,
+  'Accept': 'application/vnd.pagerduty+json;version=2'
+})
+
+# Prep response for necessary results
+User_List = pagerduty_session.get('https://api.pagerduty.com/users')
+
+# response.encoding = 'utf-8'
+if User_List.status_code != 200:
+    raise ValueError(
+        'Request to PagerDuty a server returned an error %s, the response is:\n%s'
+        % (response.status_code, response.text)
+    )
+
+#Load up the Checks found
+Check_Found = User_List.json()
+
+#Print json response info to the screen
+print(json.dumps(User_List.json(), indent=2))
